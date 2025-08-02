9a9ed7da51d4e4165b06db19bfb7fdcf010eaef7
Ptl-sneh <snehpatel641@gmail.com>
2025-08-02 17:06:10 +0530

    Made some changes in models.py

--- backend/transport/models.py
+++ backend/transport/models.py
@@ -2,37 +2,73 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# Stops for metro and brts routes
 
 class Stop(models.Model):
     name = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    order = models.PositiveIntegerField(default=0)  # Lower = comes first
-
-    class Meta:
-        ordering = ['order']
+    TYPE_CHOICES = (
+        ('bus','Bus'),
+        ('metro','Metro')
+    )
+    stop_type = models.CharField(max_length=10,choices=TYPE_CHOICES)
+    
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.stop_type}"
 
+# Bus Routes
 
 class BusRoute(models.Model):
     route_name = models.CharField(max_length=100)
-    stops = models.ManyToManyField(Stop, related_name='bus_routes')
+    stops = models.ManyToManyField(Stop, through='BusRouteStop', related_name='bus_routes')
 
     def __str__(self):
         return self.route_name
 
 
-
+# Metro Routes
 class MetroRoute(models.Model):
     route_name = models.CharField(max_length=100)
-    stops = models.ManyToManyField(Stop, related_name='metro_routes')
+    stops = models.ManyToManyField(Stop, through='MetroBusStop', related_name='metro_routes')
 
     def __str__(self):
         return self.route_name
 
+# To define brts stop in order
+class BusRouteStop(models.Model):
+    bus = models.ForeignKey(BusRoute,on_delete=models.CASCADE)
+    stop = models.ForeignKey(Stop,on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
 
+    class Meta:
+        ordering = ['order']
+        
+
+# To define metro stop in order      
+class MetroRouteStop(models.Model):
+    metro = models.ForeignKey(MetroRoute,on_delete=models.CASCADE)
+    stop = models.ForeignKey(Stop,on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+    
+    class Meta:
+        ordering = ['order']
+
+
+class BusTrip(models.Model):
+    route = models.ForeignKey(BusRoute,on_delete=models.CASCADE)
+    trip_number = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.route.route_name} Trip {self.trip_number}"
+
+
+class MetroTrip(models.Model):
+    route = models.ForeignKey(MetroRoute,on_delete = models.CASCADE)
+    trip_number = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.route.route_name} Trip {self.trip_number}"
 
 class Schedule(models.Model):
     ROUTE_TYPE_CHOICES = [
